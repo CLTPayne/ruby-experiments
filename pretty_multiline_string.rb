b06b6d6ec84db376ab3cb11f758382f0325ceee1
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-06 23:07:58 +0100

    Generic print hash as a pretty string method for future use

--- pretty_multiline_string.rb
+++ pretty_multiline_string.rb
@@ -1,3 +1,12 @@
+# Generic method template (insert relvant info around the values in the block):
+
+def to_multiline_string(hash)
+  array = hash.map { |key, value| "#{key} #{value}" }
+  array.join("\n")
+end
+
+puts to_multiline_string({"Chicken" => 2.99})
+
 # Display a pretty hash (strings as hash keys):
 
 menu = {
@@ -6,26 +15,18 @@ menu = {
   "Pepper" => 2.49,
   "Cheese" => 1.99
 }
-
 # Each returns and array:
-
 menu_dishes = menu.map { |key, value| "#{key} costs £#{value}" }
-
 # Join the array into a multiline string:
-
 puts menu_dishes.join("\n")
 
 # OR
-
 # Display a pretty hash (strings as hash keys):
-
 menu = {
   :chicken => 2.99,
   :mushroom => 1.99,
   :pepper => 2.49,
   :cheese => 1.99
 }
-
 menu_dishes = menu.map { |key, value| "#{key.capitalize} costs £#{value}" }
-
 puts menu_dishes.join("\n")
